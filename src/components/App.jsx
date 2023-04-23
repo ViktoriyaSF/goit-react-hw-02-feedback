229d6db6fe75e83350a3e8a4e52381eca5168684
ViktoriyaSF <kievstar01051985@gmail.com>
2023-04-23 13:46:44 +0300

    begin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handClick = evt => {
+    const { name, value } = evt.target;
+    // console.log('клік +1');
+    // console.log(evt.target);
+    // console.log(value);
+    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+  };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neuter + this.state.bad;
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        {Object.keys(this.state).map(name => (
+          <button key={name} name={name} onClick={this.handClick}>
+            {name}
+          </button>
+        ))}
+        <h2>Statistics</h2>
+        <ul>
+          {Object.entries(this.state).map(name => (
+            <li key={name[0]}>
+              {name[0]} : {name[1]}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+// {
+//   items.map(({ id, type, amount, currency }, index) => (
+//     <Tr key={id} index={index}>
+//       <td>{type}</td>
+//       <td>{amount}</td>
+//       <td>{currency}</td>
+//     </Tr>
+//   ));
+// }
+// {
+//   Object.keys(this.state).map(option => (
+//     <Button key={option} name={option} onClick={addFeedback}>
+//       {option}
+//     </Button>
+//   ));
+// }
+// <FeedbackOption
+//   options={Object.keys(this.state)}
+//   addFeedback={this.addFeedback}
+// />;
